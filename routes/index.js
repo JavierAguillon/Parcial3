a63fa87fee6c7b945902acf1493dab13651f2cb1
JavierAguillon <00078813@uca.edu.sv>
2019-11-23 17:28:02 -0600

    SUbiendo y finalizado

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongoose = require('mongoose')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.post('/',async (req,res)=>{
-  const {nombre , tipo, tamaño, ubicacion} = req.body;
+router.post("/", async (req, res) => {
+  const { nombre, tipo, tamaño, ubicacion } = req.body;
   const errors = [];
 
-  if(nombre.length < 0 ){
-      errors.push({text: "Nombre demasiado corto"})
+  if (nombre.length < 0) {
+    errors.push({ text: "Nombre demasiado corto" });
   }
-  if(tipo != true){
-      errors.push({text: "No se ha escrito un tipo de documento."})
+  if (tipo != true) {
+    errors.push({ text: "No se ha escrito un tipo de documento." });
   }
-  if(tamaño > 0){
-      errors.push({text: 'Su documento no tiene tamaño.'})
+  if (tamaño.length < 0) {
+    errors.push({ text: "Su documento no tiene tamaño." });
   }
-  if(errors.length > 0){
-      res.render('add_registro', {errors, phone, email, arrive_date,
-           departure_date, room, package})
-  }else{
-      const newReservation = new Reservation({firstname, lastname, phone, email,
-      arrive_date,departure_date,room,package});
-      await newReservation.save();
-      res.send('Recibido')
+  if (ubicacion.length < 0) {
+    errors.push({ text: "Su documento no tiene tamaño." });
   }
-})
-
-module.exports = router;
+  if (errors.length > 0) {
+    res.render("add_registro", {
+      errors,
+      phone,
+      email,
+      arrive_date,
+      departure_date,
+      room,
+      package
+    });
+  } else {
+    const newReservation = new Reservation({
+      firstname,
+      lastname,
+      phone,
+      email,
+      arrive_date,
+      departure_date,
+      room,
+      package
+    });
+    await newReservation.save();
+    res.send("Recibido");
+  }
+});
 
 module.exports = router;
